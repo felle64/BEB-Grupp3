ee2300334a009e58d4be84f5c168fb0809999416
felle64 <felle641@gmail.com>
2022-12-15 15:09:28 +0100

    Cleaned up api file

--- bettingModules/api.js
+++ bettingModules/api.js
@@ -3,24 +3,12 @@ const urlParams = new URLSearchParams(queryString);
 let bet = urlParams.get("bet");
 let wager = urlParams.get("wager");
 let win = urlParams.get("win");
-let betInput = document.getElementById("betInput");
-let wagerInput = document.getElementById("wagerInput");
-let betButton = document.getElementById("betButton");
-let wagerButton =document.getElementById("wagerButton");
-let bet1 = "red";
 
-//Funkar inte
-betButton.addEventListener("click",() => {
-    //console.log("Klick")
-    console.log(betInput);
+let betUser = "red";
+let wagerUser = 2;
 
-});
-wagerButton.addEventListener("click",()=>{
-    console.log(wagerInput);
-})
 
-//Funkar
-fetch(`http://80.98.140.31:5001/api/play?wager=${"2"}&bet=${bet1}`)
+fetch(`http://80.98.140.31:5001/api/play?wager=${wagerUser}&bet=${betUser}`)
 .then(res => res.json())
 .then(data => {
     console.log("dataa",data);
