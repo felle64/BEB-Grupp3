1ef14eb6843d93ae2ddafd33c0cd608d6b88b4d6
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2022-12-15 11:53:00 +0100

    Update block.js
    Cleaned up code, removed old pseudo code.

--- chainModules/block.js
+++ chainModules/block.js
@@ -1,30 +1,12 @@
-//class for block
 import calculateHash from "./calculateHash.js";
-export default class block {
-    //Has info such as
-    //index
-    //data
-    //bet values (Public)
-        //bet (red, black, odd, even)
-        //odds
-        //win
-        //payout
-        //result
-    //user (Private)
-        //username/UUID
-        //balance before and after trade?
-    //timestamp
-    //hash of previous block
-    //hash
-    //nonce (if we need this, guess it's only useful for mining?)
 
+export default class block {
     constructor(data, index = 0, prevHash = "") {
-        //A check for which index to use, either here on or the function that creates the block.
         this.index = index;
-        //The betting data in JSON format?
         this.data = data;
         this.timeStamp = Date.now();
         this.prevHash = prevHash;
         this.hash = calculateHash(this, prevHash);
+        //nonce (if we need this, guess it's only useful for mining?)
     }
 }
