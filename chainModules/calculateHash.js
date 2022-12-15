baf7e42c01f186c210881d54551e947df4d71813
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2022-12-15 11:57:28 +0100

    Update calculateHash.js
    Removed logs from troubleshooting

--- chainModules/calculateHash.js
+++ chainModules/calculateHash.js
@@ -1,20 +1,9 @@
-//Takes new block (as the block class) and previous hash value
-
 export default async function calculateHash(newBlock) {
-
     let msgInt8 = new TextEncoder().encode(
         JSON.stringify(newBlock.data) + newBlock.index + newBlock.timeStamp + newBlock.prevHash);
-    //console.log("msgInt8",msgInt8);
-
     let hashBuffer = await crypto.subtle.digest("SHA-256", msgInt8);
-    //console.log("hashBuffer",hashBuffer);
-
     let hashArray = Array.from(new Uint8Array(hashBuffer));
-   // console.log("hashArray",hashArray);
-
     let hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
-    //console.log("hashHex",hashHex);
 
-    //Returns new hash value
     return hashHex;
 }
