e85a4359fd8e719a36aa53bdcaca6948969eb705
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2022-12-16 11:42:44 +0100

    First test

--- bettingModules/api.js
+++ bettingModules/api.js
@@ -1,15 +1,24 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-let bet = urlParams.get("bet");
-let wager = urlParams.get("wager");
-let win = urlParams.get("win");
+// const queryString = window.location.search;
+// const urlParams = new URLSearchParams(queryString);
+// let bet = urlParams.get("bet");
+// let wager = urlParams.get("wager");
+// let win = urlParams.get("win");
 
-let betUser = "red";
-let wagerUser = 2;
+let wagerInput = document.getElementById("wagerInput")
+let betInput = document.getElementById("betInput")
+let betBtn = document.getElementById("betBtn")
+betBtn.addEventListener("click", () => getResult());
 
+// let betUser = "red";
+// let wagerUser = 2;
 
-fetch(`http://80.98.140.31:5001/api/play?wager=${wagerUser}&bet=${betUser}`)
-.then(res => res.json())
-.then(data => {
-    console.log("dataa",data);
-})
+function getResult() {
+    console.log(wagerInput.value);
+    console.log(betInput.value);
+    console.log(`http://80.98.140.31:5001/api/play?wager=${wagerInput.value}&bet=${betInput.value}`);
+    fetch(`http://80.98.140.31:5001/api/play?wager=${wagerInput.value}&bet=${betInput.value}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log("data", data);
+        })
+}
