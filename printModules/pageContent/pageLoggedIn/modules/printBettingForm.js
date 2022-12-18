20b9e933d3d586b0b63847565b2fdb3d679e1e79
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2022-12-18 01:46:00 +0100

    api.html => api.js
    -bettingModules:
    Renamed api.js to getResult.js to match the function name
    Deleted api.html
    -printModules/pageContent/pageLoggedIn/modules
    Added printBettingForm.js (with the HTML from api.html)
    
    Right now printBettingForm() replaces contentDiv.innerHTML, so that needs to be fixed somehow later.

---
+++ printModules/pageContent/pageLoggedIn/modules/printBettingForm.js
@@ -0,0 +1,15 @@
+export default printBettingForm(){
+
+
+
+    let contentDiv = document.getElementById('contentDiv');
+    contentDiv.innerHTML = `<div>
+<h3>Place bet</h3>
+<input type="number" id="wagerInput">
+<input type="text" id="betInput">
+<button id="betBtn">Bet</button>
+</div>`;
+
+    let betBtn = document.getElementById("betBtn");
+    betBtn.addEventListener("click", () => getResult());
+}
