d7d95f0d510c7c81428fe376e5487e976634ae63
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2022-12-18 01:57:56 +0100

    Update hashPassword.js
    Corrected function arguments and data used for msgInt8

--- userModules/hashPassword.js
+++ userModules/hashPassword.js
@@ -1,6 +1,6 @@
-export default async function hashPassword(user) {
+export default async function hashPassword(password, user) {
     let msgInt8 = new TextEncoder().encode(
-        JSON.stringify(newBlock.data) + user.uuid);
+        password + user.uuid);
     let hashBuffer = await crypto.subtle.digest("SHA-256", msgInt8);
     let hashArray = Array.from(new Uint8Array(hashBuffer));
     let hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
