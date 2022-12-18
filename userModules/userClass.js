31d321a565b9d39f58f8001d23c03f84f681b1c6
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2022-12-18 02:17:12 +0100

    Update userClass.js
    Added the extra argument needed for hashPassword

--- userModules/userClass.js
+++ userModules/userClass.js
@@ -3,7 +3,7 @@ import hashPassword from "./hashPassword.js"
 export default class userClass{
     constructor(username, password){
         this.username = username;
-        this.password = hashPassword(password);
         this.uuid = self.crypto.randomUUID();
+        this.password = hashPassword(password, this);
     }
 }
