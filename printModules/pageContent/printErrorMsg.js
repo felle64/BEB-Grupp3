3afc74ef5338a5d384ad51fbbdc64460e8cf2710
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2022-12-18 02:05:46 +0100

    Add Error Message function
    Added printErrorMsg.js
    Added a div with id "errorDiv" in index.html as a placeholder error location for now.

---
+++ printModules/pageContent/printErrorMsg.js
@@ -0,0 +1,6 @@
+export default function printErrorMsg(errorMsg){
+    let errorDiv = document.getElementById('errorDiv');
+    errorDiv.innerHTML = errorMsg;
+    //Not sure if this should be more advanced
+    //Maybe a "popup"
+}
