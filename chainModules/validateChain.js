f5f272b2d0aa1319f5e0596d9767f957764667f6
Ellen <_ellen@live.se>
2022-12-19 16:44:33 +0100

    jannes valideringskod

--- chainModules/validateChain.js
+++ chainModules/validateChain.js
@@ -2,7 +2,9 @@
 
 export default async function validateChain(){
     console.log("validerar");
-    //Fetches current chain from localStorage
+    
+       
+ //Fetches current chain from localStorage
     //For block in chain
         //check if calculateHash(block) == block.hash
             //if not
@@ -10,8 +12,27 @@ export default async function validateChain(){
                 //break
     //If all checked, and no issue
         //return valid
-}
+        //-------------------JANNES KOD--------------------------------------------------------------------------------
+        //     let testHash = currentBlock.calculateHash().then(hash => {
+        //         console.log("testHash", hash);
+        //         if (currentBlock.hash !== hash) {
+        //             console.log("INVALID! Not same hash!", currentBlock.hash, hash);
+        //             // return false;
+        //         }
+        //     });
 
+        //     if (currentBlock.prevHash !== prevBlock.hash) {
+        //         console.log("Invalid: Not same prev hash");
+        //        // return false;
+        //     }
+
+        //     console.log("VALID");
+        //    // return true;
+        // }
+        //-------------------JANNES KOD--------------------------------------------------------------------------------
+    
+
+}
 //Recalculates the hash of each block using calculateHash.js
 
 //returns Valid/Invalid (maybe including which block(s) failed validation)
