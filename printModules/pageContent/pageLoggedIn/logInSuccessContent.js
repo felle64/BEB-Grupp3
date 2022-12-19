a116a802dc01e5027cbdad6ace9267f74fb165b3
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2022-12-19 01:30:12 +0100

    Rough draft of css
    VERY rough draft just to put stuff into place.
    Redid logInSuccessContent.js a bit to allow for easier testing of CSS until we have more data to fill it with.

--- printModules/pageContent/pageLoggedIn/logInSuccessContent.js
+++ printModules/pageContent/pageLoggedIn/logInSuccessContent.js
@@ -1,16 +1,64 @@
 import buildWheel from "./modules/wheel.js";
+import printBettingForm from "./modules/printBettingForm.js";
 
 export default function logInSuccessContent() {
     let currentUser = localStorage.getItem("currentUser");
-    let innerHTML = `
-        <h2>Welcome to the page ${currentUser}!</h2>
-        <p>You have successfully logged in and are now able to enjoy this custom quote.</p>
-        <q>Wake up, ${currentUser}... <br> The Matrix has you... <br> Follow the white rabbit. 
-        <br><br><br>
-        Knock, knock, ${currentUser}.</q>
-        `;
+    let innerHTML = ""
+    
+    // innerHTML +=`
+    //     <div>
+    //     <h2>Welcome to the page ${currentUser}!</h2>
+    //     <p>You have successfully logged in and are now able to enjoy this custom quote.</p>
+    //     <q>Wake up, ${currentUser}... <br> The Matrix has you... <br> Follow the white rabbit. 
+    //     <br><br><br>
+    //     Knock, knock, ${currentUser}.</q>
+    //     </div>
+    //     `;
+
+    innerHTML += '<div id="gameDiv">';
+
+    innerHTML += '<div id="rouletteDiv">';
     //Need printBettingForm.js from PR #78
-    //innerHTML += printBettingForm()
-    innerHTML += '<div id="rouletteDiv"></div>';
+    innerHTML += '</div>';
+
+    innerHTML += printBettingForm()
+
+    innerHTML += '</div>';
+    innerHTML += '<div id="blockHistory">'
+
+    //fake blockchain data
+    innerHTML += `<div class="listBlock">
+    <p>2022-12-19 01:18</p>
+    <p>User BEEP bet 2 tokens on red</p>
+    <p>Outcome: WIN</p>
+    </div>
+    <div class="listBlock">
+    <p>2022-12-19 01:17</p>
+    <p>User BOOP bet 2 tokens on red</p>
+    <p>Outcome: WIN</p>
+    </div>
+    <div class="listBlock">
+    <p>2022-12-19 01:16</p>
+    <p>User BEEP bet 1 tokens on red</p>
+    <p>Outcome: LOSS</p>
+    </div>
+    <div class="listBlock">
+    <p>2022-12-19 01:15</p>
+    <p>User BUMPO bet 1 tokens on red</p>
+    <p>Outcome: LOSS</p>
+    </div>
+    <div class="listBlock">
+    <p>2022-12-19 01:14</p>
+    <p>User MEEPO bet 2 tokens on red</p>
+    <p>Outcome: WIN</p>
+    </div>
+    `;
+
+    innerHTML += '</div>';
+
+    
+    //let betBtn = document.getElementById("betBtn");
+    //betBtn.addEventListener("click", () => getResult());
+
     return innerHTML;
 }
--- printModules/pageContent/pageLoggedIn/modules/printBettingForm.js
+++ printModules/pageContent/pageLoggedIn/modules/printBettingForm.js
@@ -1,14 +1,11 @@
 import getResult from "../../../../bettingModules/getResult.js";
 export default function printBettingForm() {
 
-    let contentDiv = document.getElementById('contentDiv');
-    contentDiv.innerHTML = `<div>
+    let innerHTML = `<div>
 <h3>Place bet</h3>
 <input type="number" id="wagerInput">
 <input type="text" id="betInput">
 <button id="betBtn">Bet</button>
 </div>`;
-
-    let betBtn = document.getElementById("betBtn");
-    betBtn.addEventListener("click", () => getResult());
+    return innerHTML;
 }
