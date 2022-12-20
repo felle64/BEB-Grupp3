f16f8c7d278f081c71a74d4f85dd67ac00132728
felle64 <felle641@gmail.com>
2022-12-20 11:25:06 +0100

    betbtn fix

--- printModules/pageContent/pageLoggedIn/loginSuccessCode.js
+++ printModules/pageContent/pageLoggedIn/loginSuccessCode.js
@@ -1,5 +1,5 @@
 import buildWheel from "./modules/wheel.js";
-
+import betBtnEl from "./modules/betBtn.js";
 export default function loginSuccessCode() {
     buildWheel();
     let betBtn = document.getElementById("betBtn");
@@ -7,6 +7,6 @@ export default function loginSuccessCode() {
     //let betInput = document.getElementById("betInput");
     //let wagerInputValue = wagerInput.value;
     //let betInputValue = betInput.value;
-    //betBtn.addEventListener("click", betBtnEl);
+    betBtn.addEventListener("click", betBtnEl);
     //betBtnEl(wagerInput, betInput);
 }
