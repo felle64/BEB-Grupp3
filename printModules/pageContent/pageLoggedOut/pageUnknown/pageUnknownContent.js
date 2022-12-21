9dfe89190d52c02663d3c160f5b6e2422c156946
Oscar Recén Larsson <112632742+oscarrecenlarsson@users.noreply.github.com>
2022-12-21 22:17:55 +0100

    bugfix

--- printModules/pageContent/pageLoggedOut/pageUnknown/pageUnknownContent.js
+++ printModules/pageContent/pageLoggedOut/pageUnknown/pageUnknownContent.js
@@ -1,11 +1,13 @@
 export default function pageUnknownContent() {
   let pageUnknownContent = `
+  <div id="pageUnknownWrapper">
     <h2>You need to log in to see the page. </h2>
     <p>Use the form in the upper right hand corner of the screen to log in.<br>
       If you don't already have an account you can create one here.
     </p>
     <br>
     <button id="iWantToCreateAnAccountBtn">I want to create an account</button>
+    </div>
     `
   return pageUnknownContent;
 }
