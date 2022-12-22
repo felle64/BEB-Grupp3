4d3986c1c4a7b243b754fc29388c0d31e26e3cdc
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2022-12-22 03:51:41 +0100

    Implemented hashPassword in loginBtn
    printHeader.js
    Removed code from eventlistener
    Added function call for loginBtn.js
    
    loginBtn.js
    Re-wrote the code for setting logInResultSuccess to use hashPassword, and to be able to search for the new userClass

--- printModules/printHeader.js
+++ printModules/printHeader.js
@@ -1,13 +1,14 @@
 //Switch
 import validateChain from "../chainModules/validateChain.js";
 import printPage from "./printPage.js";
+import loginBtn from "../userModules/login/loginBtn.js";
 
 export default function printHeader(state) {
-    let currentUser = "";
-    if (state !== "logInSuccess") {
+  let currentUser = "";
+  if (state !== "logInSuccess") {
 
-        headerDiv.innerHTML = `<div id="validationDiv"><div id="validationResultDiv"></div><button id="validationButton">Validate</button></div>
-      <form id="logInForm">
+    headerDiv.innerHTML = `<div id="validationDiv"><div id="validationResultDiv"></div><button id="validationButton">Validate</button></div>
+      <div id="logInForm">
         <div class="inputField">
           <label for="userName">Username:</label><br>
           <input type="text" name="userName" id="userName" />
@@ -19,47 +20,30 @@ export default function printHeader(state) {
         </div>
         <br>
         <button type="submit" id="logInBtn">Log In</button>
-      </form>
+      </div>
       `
-      validateChain();
-        let logInBtn = document.getElementById("logInBtn");
-        let userName = document.getElementById("userName");
-        let userPassword = document.getElementById("userPassword");
+    validateChain();
 
-        logInBtn.addEventListener("click", (e) => {
-            e.preventDefault();
-            let users = JSON.parse(localStorage.getItem("users"));
-            let logInResultSuccess = users.find(user => user.username === userName.value && user.password === userPassword.value);
+    let logInBtn = document.getElementById("logInBtn");
 
-            if (logInResultSuccess) {
-                state = "logInSuccess";
-                localStorage.setItem("state", state);
-                currentUser = userName.value;
-                localStorage.setItem("currentUser", currentUser);
-                printPage(state);
-                printHeader(state);
-            } else {
-                state = "failedLogInAttempt";
-                localStorage.setItem("state", state);
-                printPage(state);
-                printHeader(state);
-            }
-        });
+    logInBtn.addEventListener("click", (e) => {
+      loginBtn(e);
+    });
 
-    } else {
-        currentUser = localStorage.getItem("currentUser");
-        headerDiv.innerHTML = `<div id="validationDiv"><div id="validationResultDiv"></div><button id="validationButton">Validate</button></div>
+  } else {
+    currentUser = localStorage.getItem("currentUser");
+    headerDiv.innerHTML = `<div id="validationDiv"><div id="validationResultDiv"></div><button id="validationButton">Validate</button></div>
         <p>You are logged in as: ${currentUser}</p>
         <button id="logOutBtn">Log Out</button>
         `
-        validateChain();
-        let logOutBtn = document.getElementById("logOutBtn");
-        logOutBtn.addEventListener("click", () => {
+    validateChain();
+    let logOutBtn = document.getElementById("logOutBtn");
+    logOutBtn.addEventListener("click", () => {
 
-            state = "unknown"
-            localStorage.setItem("state", state);
-            printPage(state);
-            printHeader(state);
-        });
-    };
+      state = "unknown"
+      localStorage.setItem("state", state);
+      printPage(state);
+      printHeader(state);
+    });
+  };
 };
