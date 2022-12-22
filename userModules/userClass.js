37dc96a66470dc4f997d2dc0f674667cea3316f2
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2022-12-22 03:08:28 +0100

    hashPassword rework/fix
    Added addUser.js
    Similar to addBlock, it now creates the user, and then hashes the password to add onto the user.
    UUID had the same promise issue, also moved that to addUser.js
    
    initUsers rewritten to use addUser()
    Made the whole function async, with await for addUser()

--- userModules/userClass.js
+++ userModules/userClass.js
@@ -1,10 +1,10 @@
 import hashPassword from "./hashPassword.js"
 
 export default class userClass {
-    constructor(username, password) {
+    constructor(username) {
         this.username = username;
-        this.uuid = self.crypto.randomUUID();
-        this.password = password//hashPassword(password, this)
+        //this.uuid = self.crypto.randomUUID();
+        //this.password = password//hashPassword(password, this)
             //.then( err => console.log(err));
         this.balance = 100 //100 gratis tokens när kontot skapas
         
