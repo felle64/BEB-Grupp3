0525d5773fb1003bce67a93256c06c5ebd1db9ea
felle64 <felle641@gmail.com>
2022-12-23 16:58:25 +0100

    you can win now :(

---
+++ userModules/updateUser.js
@@ -0,0 +1,11 @@
+import getUserObjectFromUserUUID from "./getUserObjectFromUserUUID.js";
+export default function updateUsers (userUpdate){
+    let users = JSON.parse(localStorage.getItem('users'))
+    let foundUser = users.find(user => {
+        return user.uuid === userUpdate.uuid;
+    })
+    let i = users.indexOf(foundUser);
+    console.log(userUpdate);
+    users[i]=userUpdate
+    localStorage.setItem('users', JSON.stringify(users))
+}
