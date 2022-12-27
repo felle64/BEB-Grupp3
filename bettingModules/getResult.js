7b5ea75fb0ec5a59c3be9067381f19a454dd0e45
felle64 <felle641@gmail.com>
2022-12-27 14:15:59 +0100

    yes

--- bettingModules/getResult.js
+++ bettingModules/getResult.js
@@ -1,7 +1,7 @@
 export default async function getResult(wagerInput, betInput) {
     console.log(wagerInput);
     console.log(betInput);
-    const apiUrl = `https://larslund.in:5001/api/play?wager=${wagerInput}&bet=${betInput}`
+    const apiUrl = `http://larslund.in:5001/api/play?wager=${wagerInput}&bet=${betInput}`
     console.log(apiUrl);
     let data = fetch(apiUrl)
         .then(res => res.json())
