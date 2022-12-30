90e1876108b1bfb0661a31e27c3a92b3c2fc097c
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2022-12-30 14:53:12 +0100

    Adding autobetter
    Added autobetter.js
    
    Imported in script.js
    
    Added users for the autobetter

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ import initPage from "./printModules/initPage.js";
 import initUsers from "./userModules/initUsers.js";
 import initBlockchain from "./chainModules/initBlockchain.js";
 import validateChain from "./chainModules/validateChain.js";
+import autoBetter from "./chainModules/mockData/autoBetter.js";
 
 await initBlockchain();
 await initUsers();
 await initPage();
 await validateChain();
+autoBetter();
